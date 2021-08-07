972d5cf6f8fea76852a28492a618623fc9200d5a
Labrege <antoine.brege92@gmail.com>
2021-08-07 19:34:40 +0200

    aze

--- encadrement.php
+++ encadrement.php
@@ -7,56 +7,116 @@
     </div>
 
     <div class="encadrement_container">
-        <div class="intoduction">
-            <div class="image">
-                <img src="image/STEPH.jpg" alt="photo_professeur">
-            </div>
-            <div class="text">  
-                <h2> Stéphane BREGEON</h2>
-                <h3>Professeur diplômé d'état 2ème degré</h3>
-                <p>
-                -Ceinture noire 6ème dan<br/><br/>
-                -Membre de l'équipe de France pendant 15 ans<br/><br/>
-                -Entraineur de nombreux champions médaillés mondiaux et olympiques.<br/><br/>
-                </p>
+        <div class="introduction">
+            <div class="txt_img">
+                <div class="intro_boite">
+                    <img src="image/STEPH.jpg" alt="photo_professeur">
+                </div>
+                <div class="intro_boite">  
+                    <h2> Stéphane BREGEON</h2>
+                    <h3>Professeur diplômé d'état 2ème degré</h3>
+                    <p>
+                    -Ceinture noire 6ème dan<br/><br/>
+                    -Membre de l'équipe de France pendant 15 ans<br/><br/>
+                    -Entraineur de nombreux champions médaillés mondiaux et olympiques.<br/><br/>
+                    </p>
+                </div>
             </div>
         </div>
         <div class="athlete">
+
             <h2>Le judo : Une passion … De grandes émotions … Un métier</h2>
             <p>
             Stéphane Brégeon commencé le Judo dès l’âge de 6 ans et l'a pratiqué avec assiduité au sein d’un seul club, l’ACBB (Athletic Club Boulogne Billancourt) où il a côtoyé des professeurs, anciens compétiteurs … et de grands champions …
             </p>
-            <div class="imageJeanluc">
-                <img src="image/STEPH.jpg" alt="photo_professeur">
-                <p>Accueil à l’aéroport de Jean-Luc ROUGE, 
-                    après son titre de Champion du Monde en 1975
-                    qui deviendra président de la fédération française de Judo pendant 15 ans de 2005 à 2020.
-                </p>
+            <div class="imagejeune">
+                <div class="txt_img">
+                    <div class="intro_boite"> 
+                        <img src="image/steph_rouge.jpg" alt="steph_rouge">
+                    </div>  
+                    <div class="intro_boite"> 
+                        <p>Accueil à l’aéroport de Jean-Luc ROUGE, après son titre de Champion du Monde en 1975
+                            qui deviendra président de la fédération française de Judo pendant 15 ans de 2005 à 2020.
+                        </p>
+                    </div>
+                </div>
+                <div class="txt_img">
+                    <div class="intro_boite"> 
+                        <img src="image/tripet.jpg" alt="tripet">
+                    </div>
+                    <div class="intro_boite"> 
+                        <p>Stéphane Brégeon avec Jean-Pierre TRIPET, 
+                        son professeur à l’école de Judo
+                        et ancien champion (75 sélections en équipe de France) la suite vice-président de la fédération française de Judo
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div class="imageTripet">
-                <img src="image/STEPH.jpg" alt="photo_professeur">
-                <p>Stéphane Brégeon avec Jean-Pierre TRIPET, 
-                son professeur à l’école de Judo
-                et ancien champion (75 sélections en équipe de France) la suite vice-président de la fédération française de Judo
-                </p>
-            </div>
+            
 
             <p>Cet environnement l’a tout naturellement conduit à poursuivre son apprentissage du judo vers la compétition ….Il passe une quinzaine d'années en équipe de France et s'entraine à l'INSEP (Institut National du Sport, de l'Expertise et de la Performance).</p>
             
             <div class="imagepodium_junior">
-                <img src="image/STEPH.jpg" alt="photo_professeur">
+                <img src="image/Europe_Junior.jpg" alt="europe junior">
                 <p>Champion de France Junior, il devient Vice-Champion d'Europe Junior à Lisbonne en 1980.</p>
             </div>
-            <div class="imagepodium_junior">
-                <img src="image/STEPH.jpg" alt="photo_professeur">
-                <p>Champion de France Junior, il devient Vice-Champion d'Europe Junior à Lisbonne en 1980.</p>
+            <div class="imagepodium_senior">
+                <img src="image/kano.png" alt="coupe kano">
+                <p>En seniors, il se classe à plusieurs reprises sur le podium des championnats de France (2ème et 3ème). Champion d'Europe par Equipes des nations et Champion du monde militaire, il participe et est médaillé aux plus prestigieux tournois internationaux tel que le Grand Slam de Paris (3ème ) ou le Grand Slam de Tokyo (3ème ). </p>
             </div>
-        </div>
-        </div>
 
+            
+        </div>
+        <div class="entraineur">
+            <p>Après une carrière de compétiteurs bien remplie, il devient professeur de judo assurant les cours enfant and différents clubs, puis entraineur et directeur technique du groupe Haut Niveau de l'ACBB, son club formateur et qui était dans les 3 meilleurs clubs français.</p>
+
+            <p> Sous sa direction, ses judokas ont remporté de nombreuses victoires et rapporté de très nombreuses médailles:
+            </p>
+            <p>Résultats par équipe : </p>
+            <ul>
+                <li> 2 fois Champions de France par équipes masculines de club </li>
+                <li> 3 participations en Coupe d'Europe des Clubs Champions</li>
+                <li> 1 fois Champion de France par équipes féminines de club </li>
+            </ul>
+            <p>Résultats individuel : </p>
+            <ul>
+                <li> 5 médailles aux Jeux Olympiques avec 8 sélectionnés (OR : 2 / ARGENT : 2 / BRONZE : 1) </li>
+                <li> 10 médailles aux Championnats du Monde (OR : 2 / ARGENT : 3 / BRONZE : 5)</li>
+                <li> 14 médailles aux Championnats d'Europe (OR : 5 / ARGENT : 4 / BRONZE : 5) </li>
+                <li> 22 médailles au GRAND SLAM de PARIS (OR : 5 / ARGENT : 6 / BRONZE : 11)</li>
+                <li> Nombreux titres de Champion de France 1ère Division </li>
+            </ul>
+
+            <div class="athlete_entrainer">
+                <div class="pascal">
+                    <img src="image/Tayot.jpg" alt="pascal Tayot">
+                    <p>Cécile NOWAK, Championne Olympique</p>
+                    <p>Pascal TAYOT, Vice-Champion Olympique</p>
+                </div>
+                <div class="Larbi">
+                    <img src="image/Larbi.jpg" alt="Larbi Benboudaoud">
+                    <p>Vice-Champion Olympique, 2x Champion du Monde -66kg</p>
+                </div>
+                <div class="cyrille">
+                    <img src="image/Maret.jpg" alt="Cyrille Maret">
+                    <p>Cyrille MARET 3ème aux JO 2016 (RIO)</p>
+                </div>
+                <div class="Pietri">
+                    <img src="image/pietri.jpg" alt="Loic Pietri">
+                    <p>Loic PIETRI Champion du monde -81 Kg</p>
+                </div>
+                <div class="Korval">
+                    <img src="image/Korval.jpg" alt="Loic Korval">
+                    <p>Loic KORVAL Champion d 'Europe, 3ème au Championnat du Monde</p>
+                </div>
+                <div class="Fabre">
+                    <img src="image/Fabre.jpg" alt="Thierry Fabre">
+                    <p>3ème Championnat du monde</p>
+                </div>
+            </div>
+
+        </div>
     </div>
-
-
     
 </div>
 
