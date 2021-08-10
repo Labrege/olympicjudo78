459d6c791b44b32f186b6c26c3ee2e71effb5f25
Labrege <antoine.brege92@gmail.com>
2021-08-10 11:05:44 +0200

    aide solal

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         </div>
     </div>
     <div class="pagination-wrapper">
-      <svg class="button btn--prev" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
+      <svg class="button_accueil btn--prev" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
         <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"/>
         <path d="M0-.5h24v24H0z" fill="none"/>
       </svg>
@@ -43,7 +43,7 @@
         <div class="little-dot  little-dot--last"></div>
       </div>
       
-      <svg class="button btn--next" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
+      <svg class="button_accueil btn--next" height="96" viewBox="0 0 24 24" width="96" xmlns="http://www.w3.org/2000/svg">
         <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"/>
         <path d="M0-.25h24v24H0z" fill="none"/>
       </svg>
