d6fad1cd4762f8fa6d2a2f497bb86ec2fbd62fc6
Labrege <61578815+Labrege@users.noreply.github.com>
2023-07-06 00:38:46 +0200

    formules

--- formules.php
+++ formules.php
@@ -25,7 +25,7 @@
                 <div class="contenue_propose">
                     <ul class="liste_contenu_plan">
                         <li class="contenu_plan"><p>Cotisation Club: 180€</p></li>
-                        <li class="contenu_plan"><p>licence assurance: 20€</p></li>
+                        <li class="contenu_plan"><p>licence assurance: 41€</p></li>
                         <li class="contenu_plan"><p>3-4 ans</p></li>
                         <li class="contenu_plan"><img src="image/tick.png" alt="tick"> <p>1 cours par semaine</p></li>
                         <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>3ans: Mardi ou Mercredi </p> </li>
@@ -49,8 +49,8 @@
                 </div>  
                 <div class="contenue_propose">
                     <ul class="liste_contenu_plan">
-                        <li class="contenu_plan"><p>Cotisation Club: 220€</p></li>
-                        <li class="contenu_plan"><p>Licence-Assurance Fédérale: 40€</p></li>
+                        <li class="contenu_plan"><p>Cotisation Club: 229€</p></li>
+                        <li class="contenu_plan"><p>Licence-Assurance Fédérale: 41€</p></li>
                         <li class="contenu_plan"><p>A partir de 5 ans</p></li>
                         <li class="contenu_plan"><img src="image/tick.png" alt="tick"> <p>2 cours par semaine </p></li>
                         <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Ju-jitsu à partir de 14 ans</p> </li>
@@ -71,16 +71,17 @@
                 style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M18.92,0.645c-0.60469,0 -1.23625,0.1075 -1.8275,0.215l-9.1375,1.72c-5.30781,0.99438 -8.74781,6.08719 -7.74,11.395l8.17,43.645c0.87344,4.6225 4.97188,7.955 9.675,7.955c0.60469,0 1.23625,0 1.8275,-0.1075l9.03,-1.72c5.29438,-0.99437 8.84188,-6.08719 7.8475,-11.395l-8.2775,-43.645c-0.87344,-4.6225 -4.86437,-8.0625 -9.5675,-8.0625zM152.9725,0.7525c-4.59562,0.08063 -8.61344,3.39969 -9.46,7.955l-8.17,43.645c-0.48375,2.56656 0.02688,5.14656 1.505,7.31c1.47813,2.16344 3.64156,3.70875 6.235,4.1925l9.03,1.6125c0.61813,0.12094 1.22281,0.1075 1.8275,0.1075c4.68969,0 8.80156,-3.3325 9.675,-7.955l8.17,-43.645c0.48375,-2.56656 -0.02687,-5.14656 -1.505,-7.31c-1.47812,-2.16344 -3.64156,-3.69531 -6.235,-4.1925l-9.03,-1.6125c-0.67187,-0.13437 -1.38406,-0.12094 -2.0425,-0.1075zM119.0025,17.3075c-2.55312,-0.09406 -4.47469,1.90813 -4.8375,4.4075c-0.06719,0.47031 -0.14781,0.92719 -0.215,1.3975c-0.60469,-0.04031 -0.84656,-0.06719 -1.505,-0.1075c-0.01344,0 -0.09406,0 -0.1075,0c-0.13437,-0.01344 -0.29562,0.01344 -0.43,0c-0.40312,-0.02687 -0.76594,-0.08062 -1.1825,-0.1075c-16.51469,-0.92719 -34.30594,-0.87344 -50.8475,0.1075c-0.59125,0.01344 -1.12875,0.08063 -1.72,0.1075c-0.81969,-5.6975 -4.03125,-5.41531 -5.375,-5.375c-3.77594,0.12094 -4.54187,2.74125 -4.6225,6.1275c-0.49719,0.05375 -1.41094,0.08063 -1.72,0.1075c-2.10969,0.20156 -4.93156,0.5375 -7.8475,0.86l1.29,6.7725c2.6875,-0.29562 5.25406,-0.56437 7.2025,-0.7525c0.17469,-0.01344 0.92719,-0.08062 1.1825,-0.1075c2.39188,51.33125 16.15188,57.31094 17.9525,61.92c0.52406,2.86219 4.4075,18.1675 4.4075,18.1675l-47.8375,48.4825c-4.31344,3.01 -4.09844,6.88 -2.9025,9.5675c1.42438,3.19813 6.54406,4.32688 10.32,1.6125l59.34,-48.2675l29.9925,5.59c0,0 -2.80844,29.25344 -3.225,33.4325c-0.41656,4.17906 0.44344,10.75 5.6975,10.75c4.74344,0 6.49031,-1.66625 8.17,-7.6325c0,0 6.67844,-40.59469 7.095,-44.505c0.44344,-4.13875 -0.72562,-5.44219 -3.9775,-6.7725c-3.25187,-1.33031 -26.445,-9.7825 -26.445,-9.7825c0,0 1.85438,-9.33906 2.15,-14.7275c4.42094,-8.82844 11.85188,-20.57281 14.835,-57.835c0.60469,0.05375 1.54531,0.17469 1.935,0.215c1.26313,0.12094 3.34594,0.36281 6.45,0.7525l1.29,-6.7725c-2.88906,-0.36281 -5.32125,-0.68531 -7.095,-0.86c-0.41656,-0.04031 -1.45125,-0.14781 -2.15,-0.215c0.41656,-3.82969 -1.43781,-6.40969 -5.2675,-6.5575zM85.785,29.1325c8.73438,0 17.44188,0.25531 25.8,0.7525c0.63156,0.04031 0.81969,0.06719 1.3975,0.1075c-3.68187,28.43375 -7.37719,52.5675 -26.9825,52.5675c-19.63219,0 -23.17969,-28.64875 -26.875,-52.675c0.22844,-0.01344 0.55094,0.01344 0.7525,0c8.39844,-0.51062 17.17313,-0.7525 25.9075,-0.7525zM86,48.16c-7.60562,0 -13.76,6.15438 -13.76,13.76c0,7.60563 6.15438,13.76 13.76,13.76c7.60563,0 13.76,-6.15437 13.76,-13.76c0,-7.60562 -6.15437,-13.76 -13.76,-13.76z"></path></g></g></svg>
                             
                 <div class="titre-formule">
-                    <h2>Judo-musculation</h2>
+                    <h2>Réduction</h2>
                      </div>
                 <div class="contenue_propose">
                     <ul class="liste_contenu_plan">
-                        <li class="contenu_plan"><p>Cotisation Club: 330€</p></li>
-                        <li class="contenu_plan"><p>Licence-Assurance Fédérale: 40€</p></li>
-                        <li class="contenu_plan"><p>A partir de 16 ans</p></li>
-                        <li class="contenu_plan"><img src="image/tick.png" alt="tick"> <p>2 cours par semaine </p></li>
-                        <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Judo, Ju-Jitsu, Taïso</p> </li>
-                        <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Accès illimité à la </br>salle de musculation</p></li>
+                        <li class="contenu_plan"><p>famille nombreuse :</br> - 10% pour le 2ème enfant</br> - 20% pour le 3ème enfant</p></li>
+                        <li class="contenu_plan"><p>Buchelois: -10% supplementaire</p></li>
+                        <li class="contenu_plan"><h3>Réglement</h3></li>
+                        <li class="contenu_plan"><img src="image/tick.png" alt="tick"> <p>La totalité à l’inscription ou en trois fois par chèques </p></li>
+                        <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Bon CAF </p> </li>
+                        <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Bon PASS'SPORT (50 €), PASS+, Chèque Sport et Chèque Vacances</p></li>
+                        <li class="contenu_plan"> <img src="image/tick.png" alt="tick"><p>Toute année commencée est due</p></li>
                         
                     </ul>
                 </div>
